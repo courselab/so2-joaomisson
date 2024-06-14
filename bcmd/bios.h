92cf44d166caa169a9792cfc395d6a639828f763
João Antônio Misson Milhorim <joao.misson@gmail.com>
2024-06-14 13:52:26 -0300

    exercise 2
    exercise 2

---
+++ bcmd/bios.h
@@ -0,0 +1,22 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2024 joaomisson <joao.misosn@gmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  joaomisson <joao.misson@gmail.com>
+ */
+
+#ifndef BIOS_H
+#define BIOS_H
+
+void __attribute__((fastcall)) clear (void);
+void __attribute__((fastcall)) print();
+void __attribute__((fastcall)) println();
+void __attribute__((fastcall)) readln(char *);
+void __attribute__((fastcall)) get_date(unsigned int*, unsigned char*, unsigned char*);
+
+
+#endif
---
+++ bcmd/utils.h
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2024 joaomisson <joao.misson@gmail.com>
+ *    SPDX-FileCopyrightText: 2024 Monaco F. J. <monaco@usp.br>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative work from SYSeg (https://gitlab.com/monaco/syseg)
+ *  and contains modifications carried out by the following author(s):
+ *  joaomisson <joao.misson@gmail.com>
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+int __attribute__((fastcall)) strcmp(const char *s1, const char *s2);
+
+#endif  /* UTILS_H  */
